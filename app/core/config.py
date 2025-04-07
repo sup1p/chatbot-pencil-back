aecd478c985ec07510f067f97e4d786a785617e6
sup1p <sup4anski@gmail.com>
2025-04-07 19:20:10 +0500

    fix pydantic problems

--- app/core/config.py
+++ app/core/config.py
@@ -3,7 +3,11 @@ import os
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
-    SECRET_KEY = os.getenv("SECRET_KEY")
-    ALGORITHM = os.getenv("ALGORITHM")
+    SECRET_KEY: str
+    ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 10
+    GEMINI_API_KEY: str
+
+    class Config:
+        env_file = ".env"
 settings = Settings()
